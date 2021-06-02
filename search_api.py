8aaba9e4b465c92017265c4f7c569e668f6ce9f7
Abhilash Raj <abhilash.raj.me@gmail.com>
2021-06-02 20:37:02 +0530

    TEST : Sample endpoint to check for search phrase query

--- search_api.py
+++ search_api.py
@@ -7,15 +7,19 @@ Module containing Backend API driver code
 
 from flask import Flask # For creating route and handling request
 from flask import request # For accessing request strings
+from dropbox_connect import get_dropbox_files # To retrieve file details from data
+from time import time
 
 # Creating Flask object
 app = Flask(__name__)
 
 @app.route('/search')
 def search():
-    search_phrase = request.args.get('q').replace('"','')
-    return f'<h1>{search_phrase}</h1>'
-
+    if 'q' in request.args:
+        search_phrase = request.args.get('q').replace('"', '')
+        return search_phrase
+    else:
+        return 'No search phrase given'
 
 if __name__ == '__main__':
     app.run(debug=True)
