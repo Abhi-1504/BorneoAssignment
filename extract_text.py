7c8acdab97f33fdf78c6c1bce4a4a5a77d3377dd
Abhilash Raj <abhilash.raj.me@gmail.com>
2021-06-02 20:22:22 +0530

    CHANGE : Removed function causing extra overhead calls

--- extract_text.py
+++ extract_text.py
@@ -1,10 +1,9 @@
 """
 @author: Abhilash Raj
 
-Module containing helper methods for extracting text from pdf and docx files
+Module containing helper functions for extracting text from pdf and docx files
 """
 
-from io import BytesIO # To store content of file in memory
 from pdftotext import PDF # For extracting text from PDF files
 from docx import Document # For extracting text from docx files
 
@@ -44,45 +43,3 @@ def extract_text_from_docx(docx_memory_file):
 
     # Returning the text
     return text
-
-
-
-def extract_text(files_data):
-    """Extract text from pdf, and docx files
-    Param(s):
-        file_data (list)        :   List of file name, file content and date
-    Return(s):
-        files_text (dict)       :   list of file data
-    """
-
-    # List to store file name and text
-    files_text = []
-    for filename, file_content, date in files_data:
-
-        # Checking the file extension
-        # For docx
-        if filename.split('.')[-1] == 'docx':
-
-            # Calling extract_text_from_docx to extract text from docx file
-            text = extract_text_from_docx(BytesIO(file_content))
-
-        # For pdf
-        elif filename.split('.')[-1] == 'pdf':
-
-            # Calling extract_text_from_pdf to extract text from pdf file
-            text = extract_text_from_pdf(BytesIO(file_content))
-
-        # For txt
-        elif filename.split('.')[-1] == 'txt':
-            text = str(file_content)
-
-        # For others
-        else:
-
-            # Setting text to unsupported in case of any other file type
-            text = 'Unsupported'
-
-        # Adding file name and text into the List
-        files_text.append([filename, text, date])
-
-    return files_text
